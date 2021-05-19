4191d030928e8f4da389e62323021e9ebfe56e43
fredThem <fdesmarais.them@gmail.com>
2021-05-18 21:44:57 -0400

    import the Task component from earlier and pass in the attributes and actions as inputs.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,1 +1,23 @@
 import React from "react";
+import Task from "./Task";
+
+export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+  const event = {
+    onPinTask,
+    onArchiveTask,
+  };
+
+  if (loading) {
+    return <div className="list-items">loading</div>;
+  }
+  if (task.lenght === 0) {
+    return <div className="list-items">empty</div>;
+  }
+  return (
+    <div className="list-items">
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} {...events} />
+      ))}
+    </div>
+  );
+}
