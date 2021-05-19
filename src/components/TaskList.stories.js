ae25f291a94947427e377ccf9fe8c6ba1270e5be
fredThem <fdesmarais.them@gmail.com>
2021-05-18 22:27:44 -0400

    Update TaskList.stories.js

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -1,27 +1,27 @@
-import React from "react";
+import React from 'react';
 
-import TaskList from "./TaskList";
-import * as TaskStories from "./Task.stories";
+import TaskList from './TaskList';
+import * as TaskStories from './Task.stories';
 
 export default {
   component: TaskList,
-  title: "TaskList",
-  decorators: [(story) => <div style={{ padding: "3rem" }}>{story()}</div>],
+  title: 'TaskList',
+  decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
 };
 
-const Template = (args) => <TaskList {...args} />;
+const Template = args => <TaskList {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
   // Shaping the stories through args composition.
   // The data was inherited from the Default story in task.stories.js.
   tasks: [
-    { ...TaskStories.Default.args.task, id: "1", title: "Task 1" },
-    { ...TaskStories.Default.args.task, id: "2", title: "Task 2" },
-    { ...TaskStories.Default.args.task, id: "3", title: "Task 3" },
-    { ...TaskStories.Default.args.task, id: "4", title: "Task 4" },
-    { ...TaskStories.Default.args.task, id: "5", title: "Task 5" },
-    { ...TaskStories.Default.args.task, id: "6", title: "Task 6" },
+    { ...TaskStories.Default.args.task, id: '1', title: 'Task 1' },
+    { ...TaskStories.Default.args.task, id: '2', title: 'Task 2' },
+    { ...TaskStories.Default.args.task, id: '3', title: 'Task 3' },
+    { ...TaskStories.Default.args.task, id: '4', title: 'Task 4' },
+    { ...TaskStories.Default.args.task, id: '5', title: 'Task 5' },
+    { ...TaskStories.Default.args.task, id: '6', title: 'Task 6' },
   ],
 };
 
@@ -31,7 +31,7 @@ WithPinnedTasks.args = {
   // Inherited data coming from the Default story.
   tasks: [
     ...Default.args.tasks.slice(0, 5),
-    { id: "6", title: "Task 6 (pinned)", state: "TASK_PINNED" },
+    { id: '6', title: 'Task 6 (pinned)', state: 'TASK_PINNED' },
   ],
 };
 
@@ -43,6 +43,8 @@ Loading.args = {
 
 export const Empty = Template.bind({});
 Empty.args = {
+  // Shaping the stories through args composition.
+  // Inherited data coming from the Loading story.
   ...Loading.args,
   loading: false,
 };
