bd7a404e24285bd5d807c5c32364d38d856e1622
fredThem <fdesmarais.them@gmail.com>
2021-06-09 09:14:10 -0400

    colors and button group

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,6 @@ import React from "react";
 // import PropTypes from "prop-types";
 import * as rbs from "react-bootstrap";
 // import { Button } from "./Button";
-import styles from "./Header.module.css";
 import BrandLogoType from "./BrandLogo";
 
 import { makeStyles } from "@material-ui/core/styles";
@@ -10,6 +9,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 import IconButton from "@material-ui/core/IconButton";
 import Link from "@material-ui/core/Link";
 // icons
@@ -19,14 +19,19 @@ import LinkedInIcon from "@material-ui/icons/LinkedIn";
 
 import { createMuiTheme } from "@material-ui/core/styles";
 import { ThemeProvider } from "@material-ui/styles";
-import deepPurple from "@material-ui/core/colors/deepPurple";
+
+import styles from "./Header.module.css";
+import purple from "@material-ui/core/colors/purple";
 // import { purple } from "@material-ui/core/colors";
 
-const theme = createMuiTheme({
+const myTheme = createMuiTheme({
   palette: {
     primary: {
-      main: deepPurple[900],
+      main: "#23036A",
     },
+  },
+  overrides: {
+    
   },
 });
 
@@ -40,16 +45,25 @@ const useStyles = makeStyles((theme) => ({
   menuButton: {
     marginRight: theme.spacing(2),
   },
+  buttonText: {},
+  buttonGroup: {
+    marginRight: theme.spacing(2),
+
+  },
   title: {
     flexGrow: 1,
+    textAlign: "right",
   },
+  groupedTextBorder: {
+  },
+  
 }));
 
 export default function Header() {
   const classes = useStyles();
   const preventDefault = (event) => event.preventDefault();
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={myTheme}>
       <div className={classes.root}>
         <AppBar position="static" color="primary">
           <Toolbar>
@@ -75,12 +89,20 @@ export default function Header() {
                 Download my CV
               </Link>
             </Typography>
-            <Button color="inherit">
-              <GitHubIcon color="inherit" />
-            </Button>
-            <Button color="inherit">
-              <LinkedInIcon color="inherit" />
-            </Button>
+            <ButtonGroup
+              variant="text"
+              // variant="contained"
+              color="secondary"
+              size="large"
+              // aria-label="text primary button group"
+            >
+              <Button color="inherit">
+                <GitHubIcon color="inherit" />
+              </Button>
+              <Button color="inherit">
+                <LinkedInIcon color="inherit" />
+              </Button>
+            </ButtonGroup>
           </Toolbar>
         </AppBar>
       </div>
@@ -95,32 +117,32 @@ const navIconStyle = {
   marginLeft: "1em",
 };
 
-export const Header_OLD = ({ user, onLogin, onLogout, onCreateAccount }) => (
-  <>
-    <rbs.Navbar
-      bg="dark"
-      variant="dark"
-      className="justify-content-between px-3"
-    >
-      <rbs.Navbar.Brand href="#home">
-        <BrandLogoType />
-      </rbs.Navbar.Brand>
-      <rbs.Nav className="ml-auto">
-        <rbs.Nav.Link href="/home">
-          <span class="hide-on-mobile">Download my </span>CV
-        </rbs.Nav.Link>
+// export const Header_OLD = ({ user, onLogin, onLogout, onCreateAccount }) => (
+//   <>
+//     <rbs.Navbar
+//       bg="dark"
+//       variant="dark"
+//       className="justify-content-between px-3"
+//     >
+//       <rbs.Navbar.Brand href="#home">
+//         <BrandLogoType />
+//       </rbs.Navbar.Brand>
+//       <rbs.Nav className="ml-auto">
+//         <rbs.Nav.Link href="/home">
+//           <span class="hide-on-mobile">Download my </span>CV
+//         </rbs.Nav.Link>
 
-        <rbs.Nav.Link eventKey="link-1">
-          <FaGithub style={navIconStyle} />
-        </rbs.Nav.Link>
+//         <rbs.Nav.Link eventKey="link-1">
+//           <FaGithub style={navIconStyle} />
+//         </rbs.Nav.Link>
 
-        <rbs.Nav.Link eventKey="link-2">
-          <FaLinkedin style={navIconStyle} />
-        </rbs.Nav.Link>
-      </rbs.Nav>
-    </rbs.Navbar>
-  </>
-);
+//         <rbs.Nav.Link eventKey="link-2">
+//           <FaLinkedin style={navIconStyle} />
+//         </rbs.Nav.Link>
+//       </rbs.Nav>
+//     </rbs.Navbar>
+//   </>
+// );
 
 Header.propTypes = {
   // user: PropTypes.shape({}),
